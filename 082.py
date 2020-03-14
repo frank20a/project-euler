99204372f582fe7034dc2f1a621777410c186625
Frank Fourlas <frank1999ipod@gmail.com>
2020-03-14 22:00:25 +0200

    082 updated

--- 082.py
+++ 082.py
@@ -1,31 +1,44 @@
-f = open('./input/p082_matrix.txt', 'r')
+f = open('input/p082_matrix.txt', 'r')
 A = []
 for line in f:
     A.append([int(i) for i in line.split(',')])
+
+
+def printer(arr):
+    for i in arr:
+        for j in i:
+            t = 2
+            if j > 999: t-=1
+            print(j, end=' '*t)
+        print()
 
 
 def solve(a):
     n = len(a)
     B = [[0 for i in range(n)] for j in range(n)]
 
-    def calc(x, y):
-        if x == n-1:
-            B[x][y] = B[x][y + 1] + A[x][y]
-        elif y == n-1:
-            B[x][y] = B[x + 1][y] + A[x][y]
-        else:
-            B[x][y] = min(B[x + 1][y], B[x][y + 1]) + A[x][y]
+    def solveColDown(x):
+        B[0][x] = A[0][x] + B[0][x + 1]
+        for i in range(1, n):
+            B[i][x] = A[i][x] + min(B[i - 1][x], B[i][x + 1])
 
+    def solveColUp(x):
+        for i in range(n - 2, -1, -1):
+            try:
+                B[i][x] = min(B[i + 1][x] + A[i][x], B[i][x])
+            except:
+                print(i)
+
+    for i in range(n): B[i][n-1] = A[i][n-1]
+    for i in range(n - 2, -1, -1):
+        solveColDown(i)
+        solveColUp(i)
+
+    res = float('inf')
     for i in range(n):
-        B[n-1][i] = A[n-1][i]
+        if B[i][0] < res: res = B[i][0]
 
-    for i in range(78, -1, -1):
-        for j in range(79, i, -1):
-            calc(i, j)
-            calc(j, i)
-            calc(i, i)
-
-    return B[0][0]
+    return res
 
 
 print(solve(A))
